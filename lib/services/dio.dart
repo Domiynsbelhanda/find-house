fb1dd962bf29007ac77de427e789c49b812a8e65
Dominique Youness <youness.dominique@gmail.com>
2022-09-09 14:46:34 +0200

    [FEAT] Dio services

---
+++ lib/services/dio.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+Dio? dio(){
+  Dio dio = new Dio();
+
+  dio.options.baseUrl = "http://10.0.2.2:8000/api";
+  //dio.options.baseUrl = "https://your_url.com/api";
+
+  dio.options.headers['accept'] = 'Application/Json';
+
+  return dio;
+}
