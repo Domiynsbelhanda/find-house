d2cd508ad014a97e02154432f3fb6013b96cc08c
Dominique Youness <youness.dominique@gmail.com>
2022-09-09 14:55:29 +0200

    [FEAT] Create env function showAlert

--- lib/utils/env.dart
+++ lib/utils/env.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:karibukwako/widget/showAlertDialog.dart';
 
 class Env {
   Color primaryColor = const Color(0xff6153ac);
   Color secondaryColor = const Color(0xff70c5d5);
+
+  showAlertDialog(BuildContext context, String titre, String description){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return ShowAlertDialog(titre, description);
+      },
+    );
+  }
 }
