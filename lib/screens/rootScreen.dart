7b0c63a697b722ad6ac47a78ef66d2f89483415f
Dominique Youness <youness.dominique@gmail.com>
2022-09-17 12:04:10 +0200

    [FEAT] Bottom rooting dispose

--- lib/screens/rootScreen.dart
+++ lib/screens/rootScreen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:karibukwako/utils/env.dart';
+
+import '../pages/homePage.dart';
 
 class RootScreen extends StatefulWidget{
+  final int? tab;
+  RootScreen({this.tab});
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -8,11 +14,43 @@ class RootScreen extends StatefulWidget{
   }
 }
 
-class _RootScreen extends State<RootScreen>{
+class _RootScreen extends State<RootScreen> with TickerProviderStateMixin{
 
   int activeTabIndex = 0;
   List barItems = [];
   int _refreshcurrIndex = 0;
+
+  late final AnimationController _controller = AnimationController(
+    duration: Duration(milliseconds: Env.ANIMATED_BODY_MS),
+    vsync: this,
+  );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller.forward();
+    activeTabIndex = widget.tab!;
+
+    barItems = [
+      {
+        "icon": "assets/icons/home.svg",
+        "page": HomePage(),
+        "text": "Accueil"
+      }
+    ];
+  }
+
+  @override
+  void dispose() {
+    _controller.stop();
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/utils/env.dart
+++ lib/utils/env.dart
@@ -5,6 +5,8 @@ class Env {
   Color primaryColor = const Color(0xff6153ac);
   Color secondaryColor = const Color(0xff70c5d5);
 
+  static const int ANIMATED_BODY_MS = 500;
+
   showAlertDialog(BuildContext context, String titre, String description){
     showDialog(
       context: context,
