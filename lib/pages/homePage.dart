e00854578a595dafed576d77c66543784200533b
Dominique Youness <youness.dominique@gmail.com>
2022-09-17 12:30:33 +0200

    [STYLE] Caroussel sliders

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/env.dart';
+
 class HomePage extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
@@ -9,9 +11,41 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePage extends State<HomePage>{
+
+  Env env = new Env();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                width: env.size(context).width,
+                height: env.size(context).height * 0.5,
+                decoration: BoxDecoration(
+
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: (env.size(context).height * 0.5) - 96,
+                      width: env.size(context).width,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(16.0)
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
