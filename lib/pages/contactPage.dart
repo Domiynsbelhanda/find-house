475cd6484e6a560a2e57ecf0c3349e4a1bc997f0
Dominique Youness <youness.dominique@gmail.com>
2022-09-22 09:34:49 +0200

    [FEAT] Card contact feating

--- lib/pages/contactPage.dart
+++ lib/pages/contactPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:karibukwako/widget/contactCard.dart';
 
 import '../utils/env.dart';
 
@@ -18,8 +20,50 @@ class _ContactPage extends State<ContactPage>{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Container(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: env.size(context).width / 1.2,
+                child: Text(
+                  'Pour tout besoin, n\'hesitez pas de nous contactactez',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: env.size(context).width / 20,
+                  ),
+                ),
+              ),
 
+              SizedBox(height: 16.0,),
+
+              ContactCard(
+                  icons: FontAwesomeIcons.phoneSlash,
+                  title: 'Numéro Téléphone',
+                  description: '+ 243 990 416 691'
+              ),
+
+              SizedBox(height: 16.0,),
+
+              ContactCard(
+                  icons: FontAwesomeIcons.mapLocationDot,
+                  title: 'Adresse physique',
+                  description: '7680, Avenue des Roches, Q/Ludo Golf, Lubumbashi'
+              ),
+
+              SizedBox(height: 16.0,),
+
+              ContactCard(
+                  icons: FontAwesomeIcons.envelope,
+                  title: 'Adresse email',
+                  description: 'info@karibukwako.com'
+              ),
+            ],
+          ),
+        )
       ),
     );
   }
---
+++ lib/widget/contactCard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ContactCard extends StatelessWidget{
+
+  IconData icons;
+  String title;
+  String description;
+
+  ContactCard({required this.icons, required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+
+    );
+  }
+}
